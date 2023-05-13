29861c75c69066144f4230e1c28480e28b12a62a
Frank Thamel <w.frankthamel@gmail.com>
2023-05-13 19:48:37 +0100

    Add CanvasPlay App

---
+++ CanvasPlay/CanvasPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasPlayApp.swift
+//  CanvasPlay
+//
+//  Created by Frank Thamel on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
